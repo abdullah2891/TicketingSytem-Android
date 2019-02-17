fdbb8d3e14050a0eca25a9703c10337babe032fd
Abdullah Rahman <abdullah2891@gmail.com>
2019-02-17 01:06:13 -0500

    fixed the ui

--- app/src/main/java/com/example/abdullahrahmn/redditview/IssuesFragment.java
+++ app/src/main/java/com/example/abdullahrahmn/redditview/IssuesFragment.java
@@ -41,7 +41,7 @@ public class IssuesFragment extends Fragment {
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
     // TODO: Customize parameters
-    private int mColumnCount = 4;
+    private int mColumnCount = 1;
     private OnListFragmentInteractionListener mListener;
     private MyIssuesRecyclerViewAdapter mAdapter;
     private List<Issue> issues;
