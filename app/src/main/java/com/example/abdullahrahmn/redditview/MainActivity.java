ffdd7e2cc33928187c0631491dc427544e076629
Abdullah Rahman <abdullah2891@gmail.com>
2019-03-02 21:56:14 -0500

    added menu button

--- app/src/main/java/com/example/abdullahrahmn/redditview/MainActivity.java
+++ app/src/main/java/com/example/abdullahrahmn/redditview/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.abdullahrahmn.redditview;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,6 +8,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +19,7 @@ import com.example.abdullahrahmn.redditview.model.Project;
 public class MainActivity extends AppCompatActivity implements ProjectFragment.OnListFragmentInteractionListener{
 
     private TextView mTextMessage;
-    private Button back;
+    private Button home;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity implements ProjectFragment.O
                     transaction.commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    back = findViewById(R.id.backButton);
-                    back.setVisibility(View.VISIBLE);
+                    home = findViewById(R.id.home);
+                    home.setVisibility(View.VISIBLE);
 
                     transaction.replace(R.id.fragment_container, new ProjectFragment());
                     transaction.commit();
@@ -49,6 +49,14 @@ public class MainActivity extends AppCompatActivity implements ProjectFragment.O
     };
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.action_menu,menu);
+        return true;
+    }
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -60,7 +68,6 @@ public class MainActivity extends AppCompatActivity implements ProjectFragment.O
             this.startActivity(new Intent(this,LoginActivity.class));
         }
 
-        backButtonHandler();
 
         //setting up the fragments
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -72,29 +79,25 @@ public class MainActivity extends AppCompatActivity implements ProjectFragment.O
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-     private void backButtonHandler(){
 
-        final Context context = this;
-        back = findViewById(R.id.backButton);
-        //hiding back button
-        back.setVisibility(View.INVISIBLE);
-        back.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()){
+            case R.id.home:
                 //setting up the fragments
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 transaction.replace(R.id.fragment_container, new ProjectFragment());
                 transaction.commit();
-                back.setVisibility(View.INVISIBLE);
-            }
-        });
-
-
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
+
     @Override
     public void onListFragmentInteraction(Project item) {
         Log.d("fragment " , item.id);
-        back.setVisibility(View.VISIBLE);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, IssuesFragment.newInstance(item.id));
         transaction.commit();
